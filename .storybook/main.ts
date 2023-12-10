91afec0793b8066a669acad688444afdcc23859d
Yutaro Kijima <eddix.ibanez@outlook.jp>
2023-12-10 21:02:23 +0900

    add RegisterForm.storie.tsx

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -14,6 +15,15 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
+  },
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        plugins: [...(config.resolve?.plugins ?? []), new TsconfigPathsPlugin()],
+      },
+    };
   },
 };
 export default config;
---
+++ src/app/auth/register/RegisterForm/RegisterForm.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta } from '@storybook/react';
+import { RegisterForm } from '.';
+
+const meta: Meta = {
+  title: 'RegisterForm',
+  component: RegisterForm,
+};
+
+export default meta;
+
+export const Component = () => (
+  <main className="container">
+    <RegisterForm />
+  </main>
+);
+Component.args = {};
