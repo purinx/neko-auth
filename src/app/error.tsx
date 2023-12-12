9fda4d29ab6c0531ff6c506bace226fb17283845
Yutaro Kijima <eddix.ibanez@outlook.jp>
2023-12-12 15:43:18 +0900

    add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+const ErrorPage = () => {
+  return (
+    <main className="container center">
+      <h1>Oops!</h1>
+      <p>Something Went Wrong</p>
+    </main>
+  );
+};
+
+export default ErrorPage;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+const ErrorPage = () => {
+  return (
+    <main className="container center">
+      <h1>404</h1>
+      <p>Not found</p>
+    </main>
+  );
+};
+
+export default ErrorPage;
