d0b69aa27e1cfc212436e1bc59beffd38b913020
Yutaro Kijima <eddix.ibanez@outlook.jp>
2023-12-12 15:03:49 +0900

    feat: Display info on dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from 'next/navigation';
 import { cookies } from 'next/headers';
 
 import { supabase } from '@/lib/supabase';
+import { DescriptionList } from '@/lib/components/layouts/DescriptionList';
 
 const DashboardPage = async () => {
   const cookie = cookies();
@@ -20,9 +21,22 @@ const DashboardPage = async () => {
     .eq('id', auth.user.id)
     .single();
 
+  if (!user) {
+    redirect('/auth/sign-in');
+  }
+
   return (
     <main className="container">
-      <h1>{user?.name} Dashboard</h1>
+      <h1>
+        {user.name}
+        {"'"}s Dashboard
+      </h1>
+      <DescriptionList>
+        <dt>name</dt>
+        <dd>{user.name}</dd>
+        <dt>email</dt>
+        <dd>{auth.user.email}</dd>
+      </DescriptionList>
     </main>
   );
 };
